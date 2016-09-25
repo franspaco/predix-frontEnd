011e0f1af1ea4361469b84b37cff5588c1146517
Ricardo Uribe <ricardo.uribe@ge.com>
2016-09-24 22:21:33 -0500

    First Commit

---
+++ public/scripts/modules/sample-module/sample-controller2.js
@@ -0,0 +1,6 @@
+define(['angular', './sample-module'], function(angular, sampleModule) {
+    'use strict';
+    return sampleModule.controller('SampleCtrl2', ['$scope', '$http', function($scope, $http) {
+        
+    }]);
+});
